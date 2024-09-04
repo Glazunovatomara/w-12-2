78407e8ee398d2217ceb98723d720eaef64f8d91
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-09-05 00:49:01 +0700

    feat:spam check

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,10 +7,15 @@ const chatList = document.querySelector('.content__comment_chat');
 const showMessage = () => {
     const firstNameValue = firstName.value;
     const avatarValue = avatar.value;
-    const commentValue = comment.value;
+    let commentValue = comment.value;
 
     const nameRight = firstNameValue[0].toUpperCase() + firstNameValue.slice(1).toLowerCase();
 
+    if ( commentValue.toLowerCase().indexOf('xxx' || commentValue.toLowerCase().indexOf('viagra')) !== -1) {
+        commentValue = commentValue.replace(/viagra/ig, '***');
+        commentValue = commentValue.replace(/xxx/ig, '***');
+    }
+    
     const templateNew = template.content.cloneNode(true);
     templateNew.getElementById('img_avatar').src = avatarValue;
     templateNew.getElementById('template_name').textContent = nameRight;
