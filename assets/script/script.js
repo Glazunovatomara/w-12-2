f0f2b975bb88a26acf0dac4a8b7095b75970e896
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-09-04 17:09:08 +0700

    fix:js

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,17 +4,17 @@ const comment = document.getElementById('comment');
 const template = document.getElementById('template');
 const chatList = document.querySelector('.content__comment_chat');
 
-const firstNameValue = firstName.value;
-const avatarValue = avatar.value;
-const commentValue = comment.value;
-
 const showMessage = () => {
-    const nameRight = firstName.value[0].toUpperCase() + firstName.value.slice(1).toLowerCase();
+    const firstNameValue = firstName.value;
+    const avatarValue = avatar.value;
+    const commentValue = comment.value;
+
+    const nameRight = firstNameValue[0].toUpperCase() + firstNameValue.slice(1).toLowerCase();
 
     const templateNew = template.content.cloneNode(true);
-    templateNew.getElementById('img_avatar').src = avatar.value;
+    templateNew.getElementById('img_avatar').src = avatarValue;
     templateNew.getElementById('template_name').textContent = nameRight;
-    templateNew.getElementById('template_text').textContent = comment.value;
+    templateNew.getElementById('template_text').textContent = commentValue;
     chatList.append(templateNew);
 
     firstName.value = '';
