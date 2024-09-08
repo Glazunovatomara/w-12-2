d6323f7947e8020d23b24c43d92261e1b90486bb
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-09-09 00:01:43 +0700

    feat:added img and input type - radio. in js added random avatar and nickname.

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,14 +3,28 @@ const avatar = document.getElementById('avatar');
 const comment = document.getElementById('comment');
 const template = document.getElementById('template');
 const chatList = document.querySelector('.content__comment_chat');
+const radio = document.getElementById('no');
+
+const hiddenBlock = () => {
+    const hidden = document.getElementById('hidden');
+
+    if(radio.checked === true) {
+        hidden.style.display = 'none'
+    } else {
+        hidden.style.display = 'flex'
+    }
+};
+
+radio.addEventListener('click', hiddenBlock())
+
 
 const showMessage = () => {
-    const firstNameValue = firstName.value;
-    const avatarValue = avatar.value;
+    let firstNameValue = firstName.value;
+    let avatarValue = avatar.value;
     let commentValue = comment.value;
 
     const currentDate = new Date();
-    
+
     const options = { 
         weekday: 'short',
         year: 'numeric',
@@ -18,9 +32,18 @@ const showMessage = () => {
         day:'numeric',
     };
     const date = currentDate.toLocaleString('ru-RU', options);
-    const time = currentDate.toLocaleTimeString('ru-RU')
+    const time = currentDate.toLocaleTimeString('ru-RU');
 
-    const nameRight = firstNameValue[0].toUpperCase() + firstNameValue.slice(1).toLowerCase();
+    if (firstNameValue === '') {
+        firstNameValue = 'Username';
+    }  else {
+        firstNameValue = firstNameValue.charAt(0).toUpperCase() + firstNameValue.slice(1).toLowerCase();
+    }
+
+    if (avatarValue === '') {
+        let random = Math.ceil(Math.random()*8)
+        avatarValue = `./assets/img/${random}.png`
+    }
 
     if (commentValue.toLowerCase().includes('xxx') || commentValue.toLowerCase().includes('viagra')) {
         commentValue = commentValue.replace(/viagra/ig, '***').replace(/xxx/ig, '***');
@@ -28,7 +51,7 @@ const showMessage = () => {
     
     const templateNew = template.content.cloneNode(true);
     templateNew.getElementById('img_avatar').src = avatarValue;
-    templateNew.getElementById('template_name').textContent = nameRight;
+    templateNew.getElementById('template_name').textContent = firstNameValue;
     templateNew.getElementById('template_text').textContent = commentValue;
     templateNew.getElementById('date').textContent = `${date.toLowerCase()} ${time}`;
     chatList.append(templateNew); 
@@ -40,3 +63,5 @@ const showMessage = () => {
 
 const button = document.getElementById('btn');
 button.addEventListener('click', showMessage);
+
+//под заголовком «Оставьте ваш комментарий» должен быть чекбокс, который даёт выбор показывать ваше имя в комментарии или нет;
