788bce6437d788dd7708e6f1e47cfd000718161d
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-09-08 18:56:25 +0700

    fix:js code

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,45 +10,15 @@ const showMessage = () => {
     let commentValue = comment.value;
 
     const currentDate = new Date();
-
-    const options = { weekday: 'short'};
-    const toDayOfWeek = currentDate.toLocaleString('ru-RU', options);
-
-    const date = currentDate.getDate();
-    const month = currentDate.getMonth();
-
-    if (month === 0) {
-        currentMonth = '01';
-    } else if (month === 1) {
-        currentMonth = '02';
-    } else if (month === 2) {
-        currentMonth = '03';
-    } else if (month === 3) {
-        currentMonth = '04';
-    } else if (month === 4) {
-        currentMonth = '05';
-    } else if (month === 5) {
-        currentMonth = '06';
-    } else if (month === 6) {
-        currentMonth = '07';
-    } else if (month === 7) {
-        currentMonth = '08';
-    } else if (month === 8) {
-        currentMonth = '09';
-    } else if (month === 9) {
-        currentMonth = '10';
-    } else if (month === 10) {
-        currentMonth = '11';
-    } else if (month === 11) {
-        currentMonth = '12';
-    } else {
-        currentMonth = 'Неизвестный месяц';
-    }
-
-    const year = currentDate.getFullYear();
-    const hours = currentDate.getHours();
-    const min = currentDate.getMinutes();
-
+    
+    const options = { 
+        weekday: 'short',
+        year: 'numeric',
+        month: 'long',
+        day:'numeric',
+    };
+    const date = currentDate.toLocaleString('ru-RU', options);
+    const time = currentDate.toLocaleTimeString('ru-RU')
 
     const nameRight = firstNameValue[0].toUpperCase() + firstNameValue.slice(1).toLowerCase();
 
@@ -60,8 +30,8 @@ const showMessage = () => {
     templateNew.getElementById('img_avatar').src = avatarValue;
     templateNew.getElementById('template_name').textContent = nameRight;
     templateNew.getElementById('template_text').textContent = commentValue;
-    templateNew.getElementById('date').textContent = `${toDayOfWeek.toUpperCase()} ${date}.${currentMonth}.${year}, ${hours}:${min}`;
-    chatList.append(templateNew);
+    templateNew.getElementById('date').textContent = `${date.toLowerCase()} ${time}`;
+    chatList.append(templateNew); 
 
     firstName.value = '';
     avatar.value = '';
